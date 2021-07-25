43d7bde8b6750e76852d2f92ba9e28a641f9adfc
quanhangbo <quanhangbo@xiaomi.com>
2021-07-25 22:23:52 +0800

    增加对数器，用批量数据验证程序是否Accept

--- src/main/java/com/xiaomi/daily_algorithm/daily01/SelectSort.java
+++ src/main/java/com/xiaomi/daily_algorithm/daily01/SelectSort.java
@@ -1,5 +1,9 @@
 package com.xiaomi.daily_algorithm.daily01;
 
+import org.springframework.util.StopWatch;
+
+import java.util.Arrays;
+
 /**
  * @author quanhangbo
  * @date 2021/7/25 18:24
@@ -21,8 +25,8 @@ public class SelectSort {
             int minIndex = i;
             for(int j = i + 1; j < arr.length; j++){
                 minIndex = arr[j] < arr[minIndex] ? j : minIndex;
-                swap(arr, i, minIndex);
             }
+	        swap(arr, i, minIndex);
         }
     }
 
@@ -30,6 +34,7 @@ public class SelectSort {
         for(int i = 0; i < arr.length; i++){
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
     }
 
     public static void swap(int[] arr, int i, int j){
@@ -37,9 +42,76 @@ public class SelectSort {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+
+    public static void comparator(int[] arr){
+        Arrays.sort(arr);
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue){
+    	// 生成长度为maxSize长度大小的数组
+    	int[] arr = new int[(int)((maxSize + 1) * Math.random())];
+    	for(int i = 0; i < arr.length; i++){
+    		arr[i] = (int) ((maxValue + 1) * Math.random() - (int) (maxValue * Math.random()));
+	    }
+    	return arr;
+    }
+
+    public static int[] copyArray(int[] arr){
+    	if(arr == null){
+    		return null;
+	    }
+    	int[] res = new int[arr.length];
+		for(int i = 0; i < arr.length; i++){
+			res[i] = arr[i];
+		}
+		return res;
+    }
+
+    public static boolean isEqual(int[] arr1, int[] arr2){
+    	if((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)){
+    		return false;
+	    }
+    	if(arr1 == null && arr2 == null){
+    		return true;
+	    }
+    	if(arr1.length != arr2.length){
+    		return false;
+	    }
+    	for(int i = 0; i < arr1.length; i++){
+    		if(arr1[i] != arr2[i]){
+    			return false;
+		    }
+	    }
+    	return true;
+    }
     public static void main(String[] args) {
-        int[] arr = {3, 2, 5, 8, 4, 7, 6, 0};
+        /*int[] arr = {3, 2, 5, 8, 4, 7, 6, 0};
         selectionSort(arr);
-        printResult(arr);
+        printResult(arr);*/
+
+        // 使用对数器模拟多台数据进行测试
+	    StopWatch stopWatch = new StopWatch();
+	    stopWatch.start();
+	    int testTime = 500000;
+	    int maxSize = 100;
+	    int maxValue = 100;
+	    boolean succeed = true;
+	    for(int i = 0; i < testTime; i++){
+	    	int[] arr1 = generateRandomArray(maxSize, maxValue);
+	    	int[] arr2 = copyArray(arr1);
+	    	selectionSort(arr1);
+	    	comparator(arr2);
+	    	if(!isEqual(arr1, arr2)){
+	    		// 如果arr1和arr2中的元素不一致，则执行该逻辑
+	    		succeed = false;
+	    		printResult(arr1);
+	    		printResult(arr2);
+	    		break;
+		    }
+	    }
+	    stopWatch.stop();
+        System.out.println("耗时：" + stopWatch.getTotalTimeMillis() + "ms");
+        System.out.println(succeed ? "Accept" : "Not Accept");
+
     }
 }
