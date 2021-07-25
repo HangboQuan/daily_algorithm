dc09cd01105e065b79f96e3369c2678ec628cb90
quanhangbo <quanhangbo@xiaomi.com>
2021-07-25 18:59:36 +0800

    修改目录结构

---
+++ src/main/java/com/xiaomi/daily_algorithm/DailyAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.xiaomi.daily_algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyAlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyAlgorithmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaomi/daily_algorithm/DailyAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaomi.daily_algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
