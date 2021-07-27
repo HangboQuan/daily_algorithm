91125b4fbb187ce860c97881e6c2ea25fdd077f7
quanhangbo <2409711401@qq.com>
2021-07-27 22:44:24 +0800

    二分查找>=Num最左边的数

---
+++ src/main/java/com/xiaomi/daily_algorithm/daily01/NearLeftValueBinarySearch.java
@@ -0,0 +1,31 @@
+package com.xiaomi.daily_algorithm.daily01;
+
+/**
+ * @author quanhangbo
+ * @date 2021/7/26 23:11
+ */
+public class NearLeftValueBinarySearch {
+
+    // 对于有序数组，找到>=某个数最左侧的位置
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 2, 2, 3, 3, 3, 4};
+        System.out.println(findNearNumLeft(arr, 2));
+    }
+
+    public static int findNearNumLeft(int[] arr, int num){
+        int index = -1;
+        int left = 0;
+        int right = arr.length - 1;
+        while(left < right){
+            int mid = left + ((right - left) >> 1);
+            if(arr[mid] >= num){
+                index = mid;
+                right = mid - 1;
+
+            }else{
+                left = mid + 1;
+            }
+        }
+        return index;
+    }
+}
